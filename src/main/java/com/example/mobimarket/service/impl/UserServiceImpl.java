8a6bff6a27b659db5f707448daa60d6151aee209
azamatshauenbekov <bravlstarstop05@mail.ru>
2023-12-29 18:37:26 +0600

    Поменял ендпоинт для отображения понравившихся продуктов

--- src/main/java/com/example/mobimarket/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/mobimarket/service/impl/UserServiceImpl.java
@@ -21,7 +21,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
